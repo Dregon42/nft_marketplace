c49a64aa0f98b27363505f011dc5cb8b64e9ec98
Dregon42 <lbrantl13@gmail.com>
2024-01-11 23:02:06 -0500

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+
+    </SafeAreaView>
+  )
+}
+
+export default Home
