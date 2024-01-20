7a7a846815d1b594508674f166e1e1d1310b497b
Dregon42 <lbrantl13@gmail.com>
2024-01-19 23:29:18 -0500

    added images of people

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -1,4 +1,5 @@
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { SIZES,FONTS, SHADOWS, assets, COLORS } from '../constants';
 
 export const NFTTitle = () => {
   return (
@@ -18,39 +19,76 @@ export const EtherPrice = () => {
     </View>
   )
 }
-export const ImageCmp = () => {
+
+// individual image of interested person
+export const ImageCmp = ({ imgUrl, index}) => {
   return (
-    <View>
-      <Text>
-        ImageCmp
-      </Text>
-    </View>
+    <Image 
+        source={imgUrl}
+        resizeMode='contain'
+        style={{
+            width: 48,
+            height: 48,
+            marginLeft: index === 0 ? 0 : -SIZES.font
+        }}
+    />
   )
 }
+
+// MAge through images of people interested in NFT
 export const People = () => {
   return (
-    <View>
-      <Text>
-        People
-      </Text>
+    <View style={{
+        flexDirection: 'row',
+    }}>
+      {[assets.person02, assets.person03, assets.person04].map((imgUrl, index) => (
+        <ImageCmp imgUrl={imgUrl} index={index} />
+      ))}
     </View>
   )
 }
+
 export const EndDate = () => {
   return (
-    <View>
-      <Text>
+    <View style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor: COLORS.white,
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: '50%'
+    }}>
+      <Text style={{
+        fontFamily: FONTS.regular,
+        fontSize: SIZES.small,
+        color: COLORS.primary
+      }}>
         EndDate
+      </Text>
+      <Text style={{
+        fontFamily: FONTS.semiBold,
+        fontSize: SIZES.medium,
+        color: COLORS.primary
+      }}>
+        12h 30m
       </Text>
     </View>
   )
 }
+
 export const SubInfo = () => {
   return (
-    <View>
-      <Text>
-        SubInfo
-      </Text>
+    <View style={{
+        width: '100%',
+        paddingHorizontal: SIZES.font,
+        marginTop: -SIZES.extraLarge,
+        flexDirection: "row",
+        justifyContent: 'space-between'
+    }}>
+      <People />
+      <EndDate />
     </View>
   )
 }
